b3bd5b51372804ddce103df011f691b29afe7293
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-19 14:49:43 +0545

    like button is imported into movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {getMovies} from "../services/fakeMovieService";
+import Like from './common/like';
 class Movies extends Component{
    state = {
        movies: getMovies()
@@ -23,6 +24,8 @@ class Movies extends Component{
                    <th>Stock</th>
                    <th>Rate</th>
                    <th></th>
+                   <th></th>
+
                </tr>
            </thead>
            <tbody>
@@ -32,6 +35,7 @@ class Movies extends Component{
                    <td>{movie.genre.name}</td>
                    <td>{movie.numberInStock}</td>
                    <td>{movie.dailyRentalRate}</td>
+                   <td><Like /></td>
                    <td><button onClick={()=>this.handleDelete(movie)} className="btn-danger btn-sm">Delete</button></td>
                </tr>
             ))}            
