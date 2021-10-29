2bd5c78c90de688139da5bed55de3084ea9e8c0c
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-29 23:12:39 +0545

    sorting for ascending and descending added to handleSort method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -45,6 +45,13 @@ class Movies extends Component {
   };
 
   handleSort = path =>{
+     const sortColumn  ={...this.state.sortColumn};
+     if (sortColumn.path === path)
+     sortColumn.order = (sortColumn.order === 'asc') ? 'desc':'asc';
+     else{
+       sortColumn.path = path;
+       sortColumn.order = 'asc';
+     }
      this.setState({ sortColumn :{path, order:'asc'}})
   };
 
