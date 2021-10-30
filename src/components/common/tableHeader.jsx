cbdc6f48523c61541f0ed157447b386d2d4916bd
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-30 12:03:50 +0545

    tableHeader is created

---
+++ src/components/common/tableHeader.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TableHeader extends Component{
+    render(){
+        return(
+            fehesu
+        );
+    }
+}
+
+export default TableHeader;
