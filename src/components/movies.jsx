d40e15a086033ca2ca564f6c19386463bce88bc7
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-30 12:01:35 +0545

    raiseSort method is updated inorder to replace the recurring of handleSort method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -44,15 +44,9 @@ class Movies extends Component {
     this.setState({selectedGenre : genre, currentPage:1});
   };
 
-  handleSort = path =>{
-     const sortColumn  ={...this.state.sortColumn};
-     if (sortColumn.path === path)
-     sortColumn.order = (sortColumn.order === 'asc') ? 'desc':'asc';
-     else{
-       sortColumn.path = path;
-       sortColumn.order = 'asc';
-     }
-     this.setState({ sortColumn :{path, order:'asc'}})
+  // here sortColumn take the sort object
+  handleSort = sortColumn =>{
+     this.setState({ sortColumn})
   };
 
   render() {
@@ -80,6 +74,7 @@ class Movies extends Component {
          
          <MoviesTable  
          movies={movies} 
+         sortColumn= {sortColumn}
          onLike= {this.handleLike} 
          onDelete ={this.handleDelete}
          onSort ={this.handleSort}
