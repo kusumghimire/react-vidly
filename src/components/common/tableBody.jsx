8cf8522cb61415f39070c438017a333bae225047
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-30 12:32:27 +0545

    mapping added to tableBody

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
-class TableBody extends Component{
-    render(){
-        return(
-            <tbody>
-                <tr>
-                    <td></td>
-                </tr>
-            </tbody>
-        );
-    }
+class TableBody extends Component {
+  render() {
+    const { data, columns } = this.props;
+    return (
+      <tbody>
+        {data.map((item) => (
+          <tr>
+            {columns.map((column) => (
+              <td></td>
+            ))}
+          </tr>
+        ))}
+        <tr></tr>
+      </tbody>
+    );
+  }
 }
 
 export default TableBody;
